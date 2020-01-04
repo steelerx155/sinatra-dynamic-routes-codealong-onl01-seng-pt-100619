2d48197e1ca974c33637aa505880429e9c644852
Randy Steele <steelerx155@gmail.com>
2020-01-04 21:02:49 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,9 @@ class App < Sinatra::Base
   "Goodbye, #{@persons_name}."
 end
 
-  get "/multiply/:num1, :num2"
-
+  get "/multiply/:num1/num2"
+  @product = params[:num1/:num2]
+  @product
+end
 
 end
